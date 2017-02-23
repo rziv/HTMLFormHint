bf09416d9bf471119f44357606a28043b9cfe9bb
YahelCh <yaelison@gmail.com>
2017-02-23 14:58:28 +0200

    adding all radiogroup cases

--- src/rules/accessibility/radio-group.js
+++ src/rules/accessibility/radio-group.js
@@ -16,19 +16,47 @@ HTMLHint.addRule({
             });
             return isRadioClassExist;
         };
-        
-        var self = this;        
-        parser.addListener('tagstart', function(event){
-            var roleAttribute = HTMLHint.utils.getAttribute(event.attrs,"role");
+        var isRadioInput = function(event){
+            var typeValue = HTMLHint.utils.getAttribute(event.attrs,"type");
+            return typeValue && typeValue.value === 'radio';
+        };
+
+        var prevEvent;
+        var self = this;   
+
+        var radioContainerNotHaveAriaLabelledbyAttr = function(event){
             var ariaLabelAttribute = HTMLHint.utils.getAttribute(event.attrs,"aria-labelledby");
-            if (isRadiogroupContainer(event) && (!roleAttribute || roleAttribute.value !== 'radiogroup'))
+            return isRadiogroupContainer(event) && (!ariaLabelAttribute || ariaLabelAttribute.value === '');
+        };
+        var radioContainerNotHaveRoleAttr = function(event){
+            var roleAttribute = HTMLHint.utils.getAttribute(event.attrs,"role");
+            return isRadiogroupContainer(event) && (!roleAttribute || roleAttribute.value !== 'radiogroup');
+        };
+        var radioNotWrpaWithDiv = function(event, prevEvent){
+            return isRadioInput(event) && prevEvent.tagName.toLowerCase() !== 'div';
+        };
+
+        var radioNotHaveRadioRole = function(event){
+            var roleAttribute = HTMLHint.utils.getAttribute(event.attrs,"role");
+            return isRadioInput(event) && (!roleAttribute || roleAttribute.value !== 'radio');
+        };
+        
+        parser.addListener('tagstart', function(event){
+            if (radioContainerNotHaveRoleAttr(event))
             {
                reporter.error('radiogroup container should have role attr with radiogroup value' + event.line , event.line, event.col, self, event.raw);
             }  
-             if (isRadiogroupContainer(event) && (!ariaLabelAttribute || ariaLabelAttribute.value === ''))
+            if (radioContainerNotHaveAriaLabelledbyAttr(event))
             {
                reporter.error('radiogroup container should have aria-labelledby attribute with value' + event.line , event.line, event.col, self, event.raw);
-            }                 
+            }   
+            if(radioNotWrpaWithDiv(event, prevEvent)){
+               reporter.error('radio input shold be wrap with div element' + event.line , event.line, event.col, self, event.raw);
+            }
+            if(radioNotHaveRadioRole(event)){
+                reporter.error('radio input shold have  role attribute with radio value' + event.line , event.line, event.col, self, event.raw);    
+            }
+            prevEvent = event;
         });
         //parser.addListener('tagend', function(event){
         //    var tagName = event.tagName.toLowerCase();          
--- test/rules/accessibility/radio-group.spec.js
+++ test/rules/accessibility/radio-group.spec.js
@@ -11,43 +11,74 @@ describe('Rules: '+ruldId, function(){
 
     it('radio group container without role of radiogroup should result in an error', function(){
         var code = `<div class="radio" aria-labelledby="radioLabel">
-                        <div><input id="private" name="type" type="radio"/></div>
-                        <div><input id="pulic" name="type" type="radio"/></div>                        
+                        <div><input id="private" name="type" type="radio" role="radio"/></div>
+                        <div><input id="pulic" name="type" type="radio" role="radio"/></div>                        
                     </div>`;
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
-       // expect(messages[0].line).to.be(6);
-        //expect(messages[0].col).to.be(33);   
     });
 
      it('radio group container with role of radiogroup should not result in an error', function(){
          var code = `<div class="radio" role="radiogroup" aria-labelledby="radioLabel">
-                        <div><input tfsdata id="private" name="type" type="radio"/></div>
-                        <div><input tfsdata id="pulic" name="type" type="radio"/></div>                        
+                        <div><input tfsdata id="private" name="type" type="radio" role="radio"/></div>
+                        <div><input tfsdata id="pulic" name="type" type="radio" role="radio"/></div>                        
                     </div>`;
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(0);
     });
     it('radiogroup container without aria-labelledby attribute should result in an error', function(){
         var code = `<div class="radio" role="radiogroup">
-                        <div><input tfsdata id="private" name="type" type="radio"/></div>
-                        <div><input tfsdata id="pulic" name="type" type="radio"/></div>                        
+                        <div><input tfsdata id="private" name="type" type="radio" role="radio"/></div>
+                        <div><input tfsdata id="pulic" name="type" type="radio" role="radio"/></div>                        
                     </div>`;
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
-
-       // expect(messages[0].line).to.be(6);
-        //expect(messages[0].col).to.be(33);   
     });
 
      it('radio group container without role of radiogroup should not result in an error', function(){
+         var code = `<div class="radio" role="radiogroup" aria-labelledby="radioLabel">
+                        <div><input id="private" name="type" type="radio" role="radio"/></div>
+                        <div><input id="pulic" name="type" type="radio" role="radio"/></div>                        
+                    </div>`;
+        var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(0);
+    });
+
+    it('radio input with role of radio should not result in an error', function(){
+        var code = `<div class="radio" role="radiogroup" aria-labelledby="radioLabel">
+                        <div><input tfsdata role="radio" id="private" name="type" type="radio"/></div>
+                        <div><input tfsdata role="radio" id="pulic" name="type" type="radio"/></div>                        
+                    </div>`;
+        var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(0);
+    });
+
+     it('radio input without role of radio should result in an error', function(){
          var code = `<div class="radio" role="radiogroup" aria-labelledby="radioLabel">
                         <div><input id="private" name="type" type="radio"/></div>
                         <div><input id="pulic" name="type" type="radio"/></div>                        
                     </div>`;
         var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(2);
+    });
+
+    it('radio input with wrapper div should not result in an error', function(){
+        var code = `<div class="radio" role="radiogroup" aria-labelledby="radioLabel">
+                        <div><input tfsdata role="radio" id="private" name="type" type="radio"/></div>
+                        <div><input tfsdata role="radio" id="pulic" name="type" type="radio"/></div>                        
+                    </div>`;
+        var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(0);
+    });
+
+     it('radio input without wrapper div should result in an error', function(){
+         var code = `<div class="radio" role="radiogroup" aria-labelledby="radioLabel">
+                        <sapn><input id="private" name="type" role="radio" type="radio"/></span>
+                        <sapn><input id="pulic" name="type" role="radio" type="radio"/></span>                        
+                    </div>`;
+        var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(2);
     });
 });
