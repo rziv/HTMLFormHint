f1a9d50b64bdb06afa3b29b9307f1dcf2c43a6c8
YahelCh <chaniyahel@gmail.com>
2017-03-20 12:04:03 +0200

    add required-wrapper-accessibility rule and tests

--- src/utils.js
+++ src/utils.js
@@ -10,6 +10,13 @@
         );
     };
 
+    const trimAll = function (val) {
+        if (typeof val !== 'string') {
+            return;
+        }
+        return val.replace(/ /g, '');
+    };
+
     const getAttribute = function (attributes, attributeName) {
         if (!Array.isArray(attributes) || typeof attributeName !== "string") {
             return undefined;
@@ -29,17 +36,36 @@
         return attributeObject ? attributeObject.value : '';
     };
 
+    const getBindingValue = function (event, bindingName) {
+        var bindings = getAttributeValue(event.attrs, "data-bind");
+        var startOfBinding = bindings.indexOf(bindingName);
+        if (startOfBinding === -1) {
+            return;
+        }
+        var endOfBinding = bindings.indexOf(',', startOfBinding) > -1 ? bindings.indexOf(',', startOfBinding) : bindings.length;
+        return bindings.substring(startOfBinding + bindingName.length + 1, endOfBinding).trim();//trim both sides to avoid whiteSpaced, add 1 to take ":" in account
+    };
+
     const isClassExsits = function (attributes, className) {
         let classNames = getAttributeValue(attributes, "class");
         let classesArray = classNames.split(/\s+/g);
         return classesArray.some(c => c === className);
     };
 
+    const isFocusableElement = function (event) {
+        var focusabaleTagNames = ['input', 'textare', 'select', 'a', 'button'];
+        return focusabaleTagNames.includes(event.tagName);
+    };
+
+
     HTMLHint.utils = {
         isAttributeExists,
         getAttribute,
+        getBindingValue,
         isClassExsits,
-        getAttributeValue
+        getAttributeValue,
+        trimAll,
+        isFocusableElement
     };
 
 })(HTMLHint);
