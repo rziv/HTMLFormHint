be97a8a0af02575a505e1cec660ecc6e7efbf00d
rachelikober <rachelikober@gmail.com>
2019-01-24 10:01:58 +0200

    fix bug and update version to 1.2.5

--- test/rules/accessibility/A-tag-accessibility.js
+++ test/rules/accessibility/A-tag-accessibility.js
@@ -76,5 +76,27 @@ describe('Rules: '+ruldId, function(){
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(0);
     });
+
+    it('A tag without "aria-label" binding should result in an error', function(){
+        var code = `<body>
+                        <span id="accessibilityNewWindowAlert" class="hide">קישור זה ייפתח בחלון חדש</span>
+                        <p>
+                        <a target="_blank" href="http://www.btl.gov.il/About/contact_us/Pages/default.aspx" data-bind="addDescription:'accessibilityNewWindowAlert'"></a>
+                        </p>
+                    </body>`;
+        var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(1);
+    });
+
+    it('A tag with "aria-label" binding should not result in an error', function(){
+        var code = `<body>
+                        <span id="accessibilityNewWindowAlert" class="hide">קישור זה ייפתח בחלון חדש</span>
+                        <p>
+                        <a target="_blank" href="http://www.btl.gov.il/About/contact_us/Pages/default.aspx" data-bind="attr:{'aria-label':labels().govil}, addDescription:'accessibilityNewWindowAlert'"></a>
+                        </p>
+                    </body>`;
+        var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(0);
+    });
 });
 
